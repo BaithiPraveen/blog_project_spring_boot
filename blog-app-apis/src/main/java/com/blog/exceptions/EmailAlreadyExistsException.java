ba03c1b7c4af63cddb77effced780c8995bf2772
Praveen Baithi <pbaithi@osius.com>
2024-01-24 10:06:19 +0530

    chore : removed unnecessary line in exception pkg

--- blog-app-apis/src/main/java/com/blog/exceptions/EmailAlreadyExistsException.java
+++ blog-app-apis/src/main/java/com/blog/exceptions/EmailAlreadyExistsException.java
@@ -1,7 +1,5 @@
 package com.blog.exceptions;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
--- blog-app-apis/src/main/java/com/blog/exceptions/ResourceNotFoundException.java
+++ blog-app-apis/src/main/java/com/blog/exceptions/ResourceNotFoundException.java
@@ -1,19 +1,11 @@
 package com.blog.exceptions;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@Getter
-@Setter
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException{
 
-    String resourceName ;
-    String fieldName;
-    Long fieldValue;
-    String stringFieldValue;
 
     public ResourceNotFoundException(String resourceName,String fieldName,Long fieldValue)
     {
